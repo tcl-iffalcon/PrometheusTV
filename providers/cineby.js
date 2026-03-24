115fae38da519afda5de33ec269234130b61c65a
tcl-iffalcon <saittcl@gmail.com>
2026-03-24 10:44:34 +0300

    Update cineby.js

--- providers/cineby.js
+++ providers/cineby.js
@@ -1,29 +1,37 @@
-// Cineby Provider for PrometheusTV
-// Embed URL pattern:
-//   Movie: https://www.cineby.gd/movie/{imdb_id}
-//   TV:    https://www.cineby.gd/tv/{imdb_id}/{season}/{episode}
+/**
+ * Cineby Provider for PrometheusTV
+ * Embed URL pattern:
+ *   Movie: https://www.cineby.gd/movie/{imdb_id}
+ *   TV:    https://www.cineby.gd/tv/{imdb_id}/{season}/{episode}
+ */
 
-var cinebyProvider = {
-  name: "Cineby",
-  timeout: 10000,
+function getStreams(tmdbid, mediatype, season, episode) {
+  var tmdbtype = mediatype === "movie" ? "movie" : "tv";
+  var tmdburl =
+    "https://api.themoviedb.org/3/" +
+    tmdbtype +
+    "/" +
+    tmdbid +
+    "?language=tr-TR&api_key=4ef0d7355d9ffb5151e987764708ce96";
 
-  getStreams: function (imdbId, type, season, episode) {
-    return new Promise(function (resolve) {
+  return fetch(tmdburl)
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (data) {
+      var imdbid = data.imdb_id || "";
+
+      if (!imdbid) return [];
+
       var embedUrl;
-
-      if (type === "movie") {
-        embedUrl = "https://www.cineby.gd/movie/" + imdbId;
+      if (mediatype === "movie") {
+        embedUrl = "https://www.cineby.gd/movie/" + imdbid;
       } else {
         embedUrl =
-          "https://www.cineby.gd/tv/" +
-          imdbId +
-          "/" +
-          season +
-          "/" +
-          episode;
+          "https://www.cineby.gd/tv/" + imdbid + "/" + season + "/" + episode;
       }
 
-      resolve([
+      return [
         {
           name: "Cineby",
           title: "▶ Cineby",
@@ -33,9 +41,8 @@ var cinebyProvider = {
             bingeGroup: "cineby",
           },
         },
-      ]);
+      ];
     });
-  },
-};
+}
 
-module.exports = cinebyProvider;
+module.exports = { getStreams };
